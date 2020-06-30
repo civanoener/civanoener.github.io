5c3cb447ffa02b29dab7d927f07d894865fc0e1c
Civan Öner <civan.oener@gmx.de>
2020-06-30 11:55:50 +0200

    Update calculator.js

--- beginnerProjects/calculator.js
+++ beginnerProjects/calculator.js
@@ -1,15 +1,18 @@
 var currentValue;
 var valueToAdd;
 var newValue; 
+var cache = [];
 
 function getValue(clicked_id) {
-        currentValue = document.getElementById("content").innerHTML;   
+        currentValue = document.getElementById("content").innerHTML;
         valueToAdd = document.getElementById(clicked_id).innerHTML;
         document.getElementById("content").innerHTML = currentValue + valueToAdd;
-    
 }
 
 function addOperation(clicked_id) {
+    cache.push(document.getElementById("content").innerHTML);
+    cache.push(clicked_id);
+    alert(cache);
     newValue = document.getElementById("cache").innerHTML + " " + document.getElementById("content").innerHTML + " " + clicked_id;
     document.getElementById("cache").innerHTML = newValue 
     document.getElementById("content").innerHTML = "";
@@ -19,3 +22,6 @@ function addOperation(clicked_id) {
 function evaluate() {
     
 }
+
+// var fruits = ["Banana", "Orange", "Apple", "Mango"];
+// fruits.push("Lemon");    // adds a new element (Lemon) to fruits
